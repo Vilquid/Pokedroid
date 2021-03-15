83e56cfaa3ef71f18b972a89df39945fe810f0b7
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-15 11:36:03 +0700

    Add a type for the pokemons
    Fire, water, electricity...

--- app/src/main/java/fr/Vilquid/NatureCollection/PokemonModel.kt
+++ app/src/main/java/fr/Vilquid/NatureCollection/PokemonModel.kt
@@ -2,10 +2,11 @@ package fr.Vilquid.NatureCollection
 
 class PokemonModel(
 	val ID: String = "pokemon_0",
-	val name: String = "Pikachu",
-	val description: String = "descrip",
+	val name: String = "Pokémon_stylé",
+	val description: String = "Description_aux petits_oignons",
 	val image_URL: String = "http://graven.yt/plante.jpg",
 	val taille: String = "Petite",
 	val puissance: String = "Faible",
+	val type: String = "Electrique",
 	var liked: Boolean = false
 )
